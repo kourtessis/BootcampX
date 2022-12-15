66de13889296442b8f7e2c76a38c0bfedff134ec
Krista Kourtessis <krista.kourtessis@telus.com>
2022-12-14 16:12:20 -0800

    node postgress walkthrough with bootcampx

---
+++ bootcamp_app/students.js
@@ -0,0 +1,53 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+// pool.connect()
+//   .then(() => {
+//     console.log('connected');
+//   });
+
+  // pool.query("select * from teachers limit 5")
+  // .then((result) => {
+  //   console.log(result.rows);
+  // });
+
+//   pool.query(`
+// SELECT id, name, cohort_id
+// FROM students
+// LIMIT 5;
+// `)
+// .then(res => {
+//   console.log(res);
+// })
+// .catch(err => console.error('query error', err.stack));
+
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// LIMIT 5;
+// `)
+// .then(res => {
+//   res.rows.forEach(user => {
+//     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//   })
+// });
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
