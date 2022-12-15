2d0d519869a46e5d66f64a340fbc0167f69a0ed2
Krista Kourtessis <krista.kourtessis@telus.com>
2022-12-14 17:14:44 -0800

    update students and teachers js files to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,6 +7,31 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`;
+
+
+const userCohortSelect = process.argv[2]
+const values = [`%${userCohortSelect}%`]
+
+pool.query(queryString, values)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
+
+
+
+
 // pool.query(`
 // SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 // FROM students
@@ -20,17 +45,3 @@ const pool = new Pool({
 //   })
 // }).catch(err => console.error('query error', err.stack));
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
